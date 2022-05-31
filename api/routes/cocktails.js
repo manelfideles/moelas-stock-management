8d1f8ecd8d10c8d97fba2470c6a406c68a50fb69
Manuel Fideles <manuelfideles77@gmail.com>
2022-06-01 00:52:00 +0100

    added list drink and list cocktail

--- api/routes/cocktails.js
+++ api/routes/cocktails.js
@@ -2,19 +2,32 @@ const { db, storage } = require('../firebase-config');
 var express = require('express');
 var router = express.Router();
 
+function getDrinkList(doc) {
+    const drinks = Object
+        .entries(doc['_fieldsProto'])
+        .map((drink) => { return { 'name': drink[0], 'quantity': drink[1]['integerValue'] } })
+    return {
+        'name': doc.id,
+        'drinks': drinks,
+    }
+}
+
 /* GET cocktails listing. */
 router.get('/', async (req, res, next) => {
     const snapshot = await db.collection('cocktails').get();
-    let cocktails = snapshot.docs.map((doc) => {
-        const drinks = Object
-            .entries(doc['_fieldsProto'])
-            .map((drink) => { return { 'name': drink[0], 'quantity': drink[1]['integerValue'] } })
-        return {
-            'name': doc.id,
-            'drinks': drinks,
-        }
-    })
-    res.send(cocktails);
+    if (snapshot) {
+        let cocktails = snapshot.docs.map((doc) => getDrinkList(doc))
+        if (cocktails) { res.status(200).send(cocktails); return; }
+    }
+    res.status(500).send('Something went wrong.');
+});
+
+/* GET specific cocktail */
+router.get('/:cocktailName', async (req, res, next) => {
+    const cocktailName = req.params.cocktailName;
+    const doc = await db.collection('cocktails').doc(cocktailName).get();
+    if (!doc.exists) res.status(404).send('That cocktail does not exist!');
+    else res.status(200).send(getDrinkList(doc))
 });
 
 module.exports = router;
--- api/routes/drinks.js
+++ api/routes/drinks.js
@@ -3,19 +3,38 @@ var express = require('express');
 const { app } = require('firebase-admin');
 var router = express.Router();
 
+const INTEGER = 'integerValue';
+const STRING = 'stringValue';
+
+function getJsonParam(json, paramName, paramType) {
+  return json['_fieldsProto'][`${paramName}`][`${paramType}`];
+}
+
 /* GET drinks listing. */
 router.get('/', async (req, res, next) => {
   const snapshot = await db.collection('drinks').get();
-  // console.log(snapshot.docs);
   let drinks = snapshot.docs.map((doc) => {
-    // console.log(doc.id, doc['_fieldsProto']);
     return {
       'name': doc.id,
-      'quantity': doc['_fieldsProto']['quantity']['integerValue'],
-      'imageUrl': doc['_fieldsProto']['imageUrl']['stringValue']
+      'quantity': getJsonParam(doc, 'quantity', INTEGER),
+      'imageUrl': getJsonParam(doc, 'imageUrl', STRING)
     }
   })
-  res.send(drinks);
+  res.status(200).send(drinks);
+});
+
+/* GET specific drink */
+router.get('/:drinkName', async (req, res, next) => {
+  const drinkName = req.params.drinkName;
+  const snapshot = await db.collection('drinks').get();
+  let drink = snapshot.docs.find((doc) => doc.id.toLowerCase() == drinkName.toLowerCase())
+  if (drink)
+    res.status(200).send({
+      'name': drinkName,
+      'quantity': getJsonParam(drink, 'quantity', INTEGER),
+      'imageUrl': getJsonParam(drink, 'imageUrl', STRING),
+    });
+  else res.status(404).send('That drink does not exist!');
 });
 
 module.exports = router;
