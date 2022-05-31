7223f00cf0f27162c8ca60870d2b82fa90f9cb6b
Manuel Fideles <manuelfideles77@gmail.com>
2022-06-01 00:09:21 +0100

    minor changes to json parsing

--- api/routes/drinks.js
+++ api/routes/drinks.js
@@ -1,5 +1,6 @@
 const { db, storage } = require('../firebase-config');
 var express = require('express');
+const { app } = require('firebase-admin');
 var router = express.Router();
 
 /* GET drinks listing. */
@@ -7,10 +8,11 @@ router.get('/', async (req, res, next) => {
   const snapshot = await db.collection('drinks').get();
   // console.log(snapshot.docs);
   let drinks = snapshot.docs.map((doc) => {
+    // console.log(doc.id, doc['_fieldsProto']);
     return {
       'name': doc.id,
       'quantity': doc['_fieldsProto']['quantity']['integerValue'],
-      'imageUrl': doc['_fieldsProto']['image_url']['stringValue']
+      'imageUrl': doc['_fieldsProto']['imageUrl']['stringValue']
     }
   })
   res.send(drinks);
